a4d97719013bd8f263eaba80ea67bfbf7219b7ae
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-26 23:23:57 +0700

    Add files via upload

--- vid2txt/vid2txt.py
+++ vid2txt/vid2txt.py
@@ -17,7 +17,6 @@ def transcribe_audio(audio_file):
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript['text']
 
-
 st.title("YouTube Video Transcription")
 
 # Get YouTube video URL from user input
@@ -33,25 +32,17 @@ if video_url.startswith("https://www.youtube.com/") or video_url.startswith("htt
         stream = yt.streams.filter(only_audio=True).first()
 
         # Save the file with the video title
-        st.write("Save the file with the video title... Please wait.")
+        st.write("Converting video to audio... Please wait.")
         temp_file_1 = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
         stream.download(output_path=temp_file_1.name, filename=f"{video_title}.mp4")
         temp_file_1.close()
 
-        # Transcribe the audio
-        st.write("Transcribing video... Please wait.")
-        transcript = transcribe_audio(temp_file_1)
-
-        # Display the transcript
-        st.subheader("Transcript:")
-        st.write(transcript)
-
-        # Download transcript as a text file
+        # Download audio file
         st.download_button(
-            label="Download Transcript",
-            data=BytesIO(transcript.encode("utf-8")),
-            file_name=f"{video_title}_transcript.txt",
-            mime="text/plain",
+            label="Download Audio",
+            data=open(temp_file_1.name, "rb"),
+            file_name=f"{video_title}_audio.mp4",
+            mime="audio/mp4",
         )
 
         # Clean up the temporary files
@@ -59,6 +50,31 @@ if video_url.startswith("https://www.youtube.com/") or video_url.startswith("htt
 
     except Exception as e:
         st.error(f"Error: {e}")
-
+        
 else:
     st.warning("Please enter a valid YouTube URL.")
+
+    st.subheader("Upload Audio and Transcribe")
+
+    uploaded_audio = st.file_uploader("Upload the audio file:")
+
+if uploaded_audio:
+    try:
+    # Transcribe the audio
+    st.write("Transcribing audio... Please wait.")
+    transcript = transcribe_audio(uploaded_audio)
+    
+    # Display the transcript
+    st.subheader("Transcript:")
+    st.write(transcript)
+
+    # Download transcript as a text file
+    st.download_button(
+        label="Download Transcript",
+        data=BytesIO(transcript.encode("utf-8")),
+        file_name="transcript.txt",
+        mime="text/plain",
+    )
+
+    except Exception as e:
+        st.error(f"Error: {e}")
