7691f3b580fb59de704a41b30b6467c348ada7c7
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-26 21:12:23 +0700

    Add files via upload

--- vid2txt/vid2txt.py
+++ vid2txt/vid2txt.py
@@ -33,4 +33,36 @@ st.title("YouTube Video Transcription")
 video_url = st.text_input("Enter YouTube video URL:")
 
 # Check if the input is a valid YouTube URL
-if video_url.startswith("https://www.youtube.com/") or video_url.startswith("
+if video_url.startswith("https://www.youtube.com/") or video_url.startswith("https://youtu.be/"):
+    
+    try:
+        # Load the YouTube video
+        youtube_video = YouTube(video_url)
+        st.write(f"Video title: {youtube_video.title}")
+
+        # Filter to get only audio streams and select the first one
+        streams = youtube_video.streams.filter(only_audio=True)
+        stream = streams.first()
+
+        # Download the audio stream to memory
+        audio_data = BytesIO()
+        stream.stream_to_buffer(audio_data)
+        audio_data.seek(0)
+
+        # Transcribe the audio data
+        with st.spinner("Transcribing YouTube video..."):
+            transcript = transcribe_audio(audio_data)
+
+        st.subheader("Transcription")
+        st.write(transcript)
+
+        # Create a download button for the transcription
+        if st.button("Download Transcription"):
+            st.download_button(
+                "Download Transcription", data=transcript, file_name="transcription.txt"
+            )
+    except Exception as e:
+        st.error(f"Error: {e}")
+else:
+    if video_url:
+        st.error("Invalid YouTube URL. Please enter a valid URL.")
