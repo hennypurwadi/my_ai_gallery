466e25483dc5ed964726f4063a6520b73aa93626
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-26 22:38:23 +0700

    Add files via upload

--- vid2txt/vid2txt.py
+++ vid2txt/vid2txt.py
@@ -12,19 +12,10 @@ with open('api_key.txt', 'r') as f:
     api_key = f.read().strip()
 openai.api_key = api_key
 
-# Convert mp4 to wav
-def mp4_to_wav(mp4_file):
-    audio = AudioSegment.from_file(mp4_file.name, format="mp4")
-    wav_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
-    audio.export(wav_file.name, format="wav")
-    return wav_file.name
-
 # Transcribe the audio file using Whisper
 def transcribe_audio(audio_file):
-    wav_file = mp4_to_wav(audio_file)
-    with open(wav_file, "rb") as file:
+    with open(audio_file.name, "rb") as file:
         transcript = openai.Audio.transcribe("whisper-1", file)
-    os.unlink(wav_file)
     return transcript['text']
 
 st.title("YouTube Video Transcription")
