17c6ee509a63cd93a9beff441652bd04e3c53a27
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-04-02 09:52:57 +0700

    Add files via upload

--- auto_classify.py
+++ auto_classify.py
@@ -58,7 +58,8 @@ def main():
     openai.api_key = api_key
 
     # user to upload a file
-    file = st.file_uploader("Upload less than 100 rows of .csv, or .xlsx file which has 'text' column", type=["csv", "xlsx"])
+    file = st.file_uploader("Upload a .csv or .xlsx file with no more than 100 rows. The file must contain a 'text' column.", 
+                            type=["csv", "xlsx"])
 
     # user to input up to 6 categories
     categories = st.text_input("Enter up to 6 categories separated by commas", "")
